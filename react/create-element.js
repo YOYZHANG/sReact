321d9a3ef0dba2caa21c10264f5b0b8035b76d4d
YOYZHANG <alexuzhang@gmail.com>
2020-12-30 19:54:41 +0800

    diff

--- react/create-element.js
+++ react/create-element.js
@@ -1,11 +1,12 @@
-import Component from './component.js'
-
 function createElement( tag, attrs, ...children ) {
+
+    attrs = attrs || {};
 
     return {
         tag,
         attrs,
-        children
+        children,
+        key: attrs.key || null
     }
 }
 
